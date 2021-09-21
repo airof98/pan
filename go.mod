2914ada3701519c292c6f26624256a5984ab0972
heesu.shin <airof98@gmail.com>
2021-09-22 07:53:51 +0900

    add utm

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/golang/geo v0.0.0-20210211234256-740aa86cb551
+	github.com/im7mortal/UTM v1.2.0
 	github.com/pebbe/go-proj-4 v5.0.0+incompatible
 	github.com/stretchr/testify v1.7.0
 )
--- proj/proj_test.go
+++ proj/proj_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/airof98/pan/com"
 
+	"github.com/im7mortal/UTM"
 	"github.com/pebbe/go-proj-4/proj"
 	"github.com/stretchr/testify/assert"
 )
@@ -22,8 +23,12 @@ const (
 	epsg5179 = "+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=1000000 +y_0=2000000 +ellps=GRS80 +units=m +no_defs"
 	//kakao
 	epsg5181 = "+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=200000 +y_0=500000 +ellps=GRS80 +units=m +no_defs"
-	lng      = 127.028126674
-	lat      = 37.499143848
+	//test
+	epsgTest = "+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs"
+	//epsgTest = "+proj=tmerc +lat_0=7.000480277777778 +lon_0=80.77171111111112 +k=0.9999238418 +x_0=200000 +y_0=200000 +a=6377276.345 +b=6356075.41314024 +towgs84=-97,787,86,0,0,0,0 +units=m +no_defs"
+	//epsgTest = "+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs"
+	lng = 127.028126674
+	lat = 37.499143848
 )
 
 var (
@@ -31,6 +36,7 @@ var (
 	p4326 *proj.Proj
 	p5179 *proj.Proj
 	p5181 *proj.Proj
+	pTest *proj.Proj
 )
 
 func init() {
@@ -38,6 +44,7 @@ func init() {
 	p4326, _ = proj.NewProj(epsg4326)
 	p5179, _ = proj.NewProj(epsg5179)
 	p5181, _ = proj.NewProj(epsg5181)
+	pTest, _ = proj.NewProj(epsgTest)
 }
 
 func TestGoogleToLnglat(t *testing.T) {
@@ -57,6 +64,22 @@ func TestLnglatToKakao(t *testing.T) {
 	x, y, err := proj.Transform2(p4326, p5181, proj.DegToRad(lng), proj.DegToRad(lat))
 	assert.NoError(t, err)
 	fmt.Printf("https://map.kakao.com/?map_type=TYPE_MAP&q=test&urlLevel=3&urlX=%d&urlY=%d\n", int(x*2.5), int(y*2.5))
+
+	x, y, err = proj.Transform2(p4326, pTest, proj.DegToRad(127), proj.DegToRad(38))
+	assert.NoError(t, err)
+	fmt.Println(x, y)
+
+	x, y, err = proj.Transform2(p4326, p5181, proj.DegToRad(127), proj.DegToRad(38))
+	assert.NoError(t, err)
+	fmt.Println(x, y)
+
+	x, y, err = proj.Transform2(p4326, pTest, proj.DegToRad(127.5), proj.DegToRad(38))
+	assert.NoError(t, err)
+	fmt.Printf("%.7f,%.7f\n", x, y)
+
+	x, y, zn, zl, err := UTM.FromLatLon(lat, lng, false)
+	assert.NoError(t, err)
+	fmt.Println(x, y, zn, zl)
 }
 
 func TestDistance(t *testing.T) {
@@ -71,7 +94,7 @@ func TestDistance(t *testing.T) {
 
 	x0, y0, err = proj.Transform2(p4326, p3857, proj.DegToRad(lng0), proj.DegToRad(lat0))
 	assert.NoError(t, err)
-	x1, y1, err = proj.Transform2(p4326, p3857, proj.DegToRad(lng1), proj.DegToRad(lat1))
+	x1, y1, err = proj.Transform2(p4326, pTest, proj.DegToRad(lng1), proj.DegToRad(lat1))
 	assert.NoError(t, err)
 	d = com.EuclidDistance(x0, y0, x1, y1)
 	fmt.Println(d)
